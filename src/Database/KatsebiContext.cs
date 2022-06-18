124ada160ca0055d87df0a67812abe419ee7d4f9
revazashvili <levanrevazashvili@gmail.com>
2022-06-18 12:47:24 +0400

    add KatsebiContext.cs

---
+++ src/Database/KatsebiContext.cs
@@ -0,0 +1,15 @@
+using Database.Entities;
+
+namespace Database;
+
+public class KatsebiContext : DbContext
+{
+    public KatsebiContext(DbContextOptions<KatsebiContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Playlist> Playlists { get; set; }
+    public DbSet<Episode> Episodes { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Quote> Quotes { get; set; }
+}
