ac436682f29e97c31e9d9d5d11b12a5c677284e6
revazashvili <levanrevazashvili@gmail.com>
2022-06-18 12:28:23 +0400

    add PlaylistConfiguration.cs

---
+++ src/Database/Configurations/PlaylistConfiguration.cs
@@ -0,0 +1,22 @@
+using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configurations;
+
+public class PlaylistConfiguration : IEntityTypeConfiguration<Playlist>
+{
+    public void Configure(EntityTypeBuilder<Playlist> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Property(x => x.Name).HasMaxLength(100).IsRequired().IsUnicode();
+        builder.HasIndex(x => x.Name);
+        
+        builder.HasMany(x => x.Episodes)
+            .WithOne(x => x.Playlist)
+            .HasForeignKey(x => x.PlaylistId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
