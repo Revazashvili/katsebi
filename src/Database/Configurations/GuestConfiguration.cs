87a3c55c988bfbc3f52a314859fc1577a519e332
revazashvili <levanrevazashvili@gmail.com>
2022-06-18 12:37:59 +0400

    add index on Guest name

--- src/Database/Configurations/GuestConfiguration.cs
+++ src/Database/Configurations/GuestConfiguration.cs
@@ -11,6 +11,7 @@ public class GuestConfiguration : IEntityTypeConfiguration<Guest>
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Property(x => x.Name).HasMaxLength(100).IsRequired().IsUnicode();
+        builder.HasIndex(x => x.Name);
         builder.Property(x => x.Description).HasMaxLength(500).IsRequired(false).IsUnicode();
 
         builder.HasMany(x => x.Episodes)
