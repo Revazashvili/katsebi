8f99ef1b3149f9064d960af4a9e7847ad034f9c5
revazashvili <levanrevazashvili@gmail.com>
2022-10-03 00:42:22 +0400

    move default schema name in const variable

--- src/API/Persistence/KatsebiContext.cs
+++ src/API/Persistence/KatsebiContext.cs
@@ -5,6 +5,7 @@ namespace API.Persistence;
 #pragma warning disable CS8618
 public class KatsebiContext : DbContext
 {
+    private const string DefaultSchemaName = "katsebi";
     public KatsebiContext(DbContextOptions<KatsebiContext> options) : base(options) { }
     
     public DbSet<Playlist> Playlists { get; set; }
@@ -14,7 +15,7 @@ public class KatsebiContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.HasDefaultSchema("katsebi");
+        builder.HasDefaultSchema(DefaultSchemaName);
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(builder);
     }
