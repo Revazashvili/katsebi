c5d21a3752a548a48439910971c52edac190f632
revazashvili <levanrevazashvili@gmail.com>
2022-10-03 00:52:53 +0400

    add docs in ServiceCollectionExtensions

--- src/API/Persistence/ServiceCollectionExtensions.cs
+++ src/API/Persistence/ServiceCollectionExtensions.cs
@@ -1,8 +1,17 @@
 namespace API.Persistence;
 
-public static class ServiceCollectionExtensions
+/// <summary>
+/// Contains methods to configure persistence layer.
+/// </summary>
+internal static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString) =>
+    /// <summary>
+    /// Add Persistence layer in IoC Container.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add persistence to.</param>
+    /// <param name="connectionString">Database Connection String.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    internal static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString) =>
         services.AddDbContextPool<KatsebiContext>(builder =>
         {
             builder.UseSqlite(connectionString, optionsBuilder =>
@@ -12,6 +21,11 @@ public static class ServiceCollectionExtensions
             });
         }).AutoMigrate();
 
+    /// <summary>
+    /// Auto migrates schema changes to the database.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to retrieve <see cref="KatsebiContext"/> instance.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
     private static IServiceCollection AutoMigrate(this IServiceCollection services)
     {
         var serviceProvider = services.BuildServiceProvider();
